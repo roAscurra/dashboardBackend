9765539049e88fd783aa0b211d88eee67e6ff689
Constanza <Unoconstanza@gmail.com>
2024-04-26 09:38:37 -0300

    subocambio

--- src/main/java/com/example/dashboard/entities/ArticuloManufacturado.java
+++ src/main/java/com/example/dashboard/entities/ArticuloManufacturado.java
@@ -19,9 +19,7 @@ public class ArticuloManufacturado extends Base{
     @Column(name = "preparacion")
     private String preparacion;
 
-    @OneToMany
-    @JoinColumn(name="articulo")
-    private Articulo articulo;
+
 
 
 }
