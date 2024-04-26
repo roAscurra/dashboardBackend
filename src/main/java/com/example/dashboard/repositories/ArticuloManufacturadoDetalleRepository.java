15dd9c30d10ad40ff36fe84d936772d264c189d6
Constanza <Unoconstanza@gmail.com>
2024-04-26 08:22:39 -0300

    suboInterfaces

---
+++ src/main/java/com/example/dashboard/repositories/ArticuloManufacturadoDetalleRepository.java
@@ -0,0 +1,8 @@
+package com.example.dashboard.repositories;
+
+import com.example.dashboard.entities.ArticuloManufacturadoDetalle;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticuloManufacturadoDetalleRepository extends BaseRepository<ArticuloManufacturadoDetalle, Long>{
+}
---
+++ src/main/java/com/example/dashboard/repositories/UnidadMedidaRepository.java
@@ -0,0 +1,8 @@
+package com.example.dashboard.repositories;
+
+import com.example.dashboard.entities.UnidadMedida;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnidadMedidaRepository extends BaseRepository<UnidadMedida, Long>{
+}
